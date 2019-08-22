9138a7e0dfa802cc7257662a399a7d10adc06eed
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2019-08-22 11:19:00 +0300

    Update cards_war.py

--- cards_war.py
+++ cards_war.py
@@ -12,11 +12,12 @@ class player():
         self.name = name
         self.nr_cards = nr_cards
         self.cards = []
-        self.mana_razboi=[]
+        self.war_hand = []
+
 
 numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15]
-type_cards = ['romb', 'trifoi', 'inima', 'pica']
-player1 = player("Mircea", 0)
+type_cards = ['diamond', 'clover', 'hearts', 'spades']
+player1 = player("player 1", 0)
 card_picked = card(random.choice(numbers), random.choice(type_cards))
 player1.cards.append(card_picked)
 while (player1.nr_cards < 25):
@@ -30,7 +31,7 @@ while (player1.nr_cards < 25):
         player1.cards.append(card_picked)
         player1.nr_cards += 1
 
-player2 = player("Alex", 0)
+player2 = player("player 2", 0)
 card_is_in_hand = 1
 while card_is_in_hand == 1:
     card_picked = card(random.choice(numbers), random.choice(type_cards))
@@ -59,50 +60,50 @@ while (player2.nr_cards < 25):
         player2.cards.append(card_picked)
         player2.nr_cards += 1
 
-print("Cartile au fost impartite")
-while(player1.nr_cards>0 and player2.nr_cards>0):
+print("Cards have been split")
+while (player1.nr_cards > 0 and player2.nr_cards > 0):
     card_on_filed1 = random.choice(player1.cards)
     card_on_filed2 = random.choice(player2.cards)
     player1.cards.remove(card_on_filed1)
     player2.cards.remove(card_on_filed2)
-    player1.mana_razboi.append(card_on_filed1)
-    player2.mana_razboi.append(card_on_filed2)
+    player1.war_hand.append(card_on_filed1)
+    player2.war_hand.append(card_on_filed2)
 
     if card_on_filed1.number > card_on_filed2.number:
-        print(card_on_filed1.number, " este mai mare", card_on_filed2.number)
-        print("Carti luate de playerul ",player1.name)
-        player1.cards.extend(player2.mana_razboi)
-        player1.cards.extend(player1.mana_razboi)
-        player2.nr_cards=player2.nr_cards-len(player2.mana_razboi)
-        player1.nr_cards=player1.nr_cards+len(player1.mana_razboi)
-        player2.mana_razboi=[]
-        player1.mana_razboi=[]
-    elif (card_on_filed1.number<card_on_filed2.number):
-        print(card_on_filed2.number, " este mai mare", card_on_filed1.number)
-        print("Carti luate de playerul ",player2.name)
-        player2.cards.extend(player2.mana_razboi)
-        player1.cards.extend(player1.mana_razboi)
-        player2.nr_cards=player2.nr_cards+len(player2.mana_razboi)
-        player1.nr_cards=player1.nr_cards-len(player1.mana_razboi)
-        player2.mana_razboi=[]
-        player1.mana_razboi=[]
+        print(card_on_filed1.number, " is bigger", card_on_filed2.number)
+        print("Cards taken by player: ", player1.name)
+        player1.cards.extend(player2.war_hand)
+        player1.cards.extend(player1.war_hand)
+        player2.nr_cards = player2.nr_cards - len(player2.war_hand)
+        player1.nr_cards = player1.nr_cards + len(player1.war_hand)
+        player2.war_hand = []
+        player1.war_hand = []
+    elif (card_on_filed1.number < card_on_filed2.number):
+        print(card_on_filed2.number, "is bigger", card_on_filed1.number)
+        print("Cards taken by player: ", player2.name)
+        player2.cards.extend(player2.war_hand)
+        player1.cards.extend(player1.war_hand)
+        player2.nr_cards = player2.nr_cards + len(player2.war_hand)
+        player1.nr_cards = player1.nr_cards - len(player1.war_hand)
+        player2.war_hand = []
+        player1.war_hand = []
     else:
-        for i in range(0,card_on_filed2.number-1):
+        for i in range(0, card_on_filed2.number - 1):
             card_on_filed1 = random.choice(player1.cards)
             card_on_filed2 = random.choice(player2.cards)
-            player1.mana_razboi.append(card_on_filed1)
+            player1.war_hand.append(card_on_filed1)
             player1.cards.remove(card_on_filed1)
-            player2.mana_razboi.append(card_on_filed2)
+            player2.war_hand.append(card_on_filed2)
             player2.cards.remove(card_on_filed2)
 
     for card_in_hand in player1.cards:
-        print(card_in_hand.number," ",card_in_hand.card_type,end=';')
+        print(card_in_hand.number, " ", card_in_hand.card_type, end=';')
     print()
     for card_in_hand in player2.cards:
-        print(card_in_hand.number," ",card_in_hand.card_type,end=';')
+        print(card_in_hand.number, " ", card_in_hand.card_type, end=';')
     print()
 print()
-if player2.nr_cards<0:
-    print(player1.name," a castigat")
+if player2.nr_cards < 0:
+    print(player1.name, " won")
 else:
-    print(player2.name," a castigat")
+    print(player2.name, " won")
