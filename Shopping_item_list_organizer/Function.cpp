12df201529d8b6e7a3ce4532eb08b354ffce7b26
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2020-09-30 10:35:06 +0300

    Update Function.cpp

--- Shopping_item_list_organizer/Function.cpp
+++ Shopping_item_list_organizer/Function.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include<iostream>
 using namespace std;
-Item::Item(std::string data,int item_price)
+Item::Item(std::string data, int item_price)
 {
     item_name = data;
     price = item_price;
@@ -15,20 +15,20 @@ Item::Item()
 }
 BST::BST()
 {
-	item=Item();
-	left = right = NULL;
+    item = Item();
+    left = right = NULL;
 }
 BST::BST(Item data)
 {
     item = Item(data.item_name, data.price);
-	left = right = NULL;
+    left = right = NULL;
 }
-BST* BST::Insert(BST* root,Item data)
+BST* BST::Insert(BST* root, Item data)
 {
-	if (!root)
-	{
-		return new BST(data);
-	}
+    if (!root)
+    {
+        return new BST(data);
+    }
     if (data.price > root->item.price)
     {
         root->right = Insert(root->right, data);
@@ -52,34 +52,31 @@ BST* BST::Preorder(BST* root)
         Preorder(root->right);
     }
 }
-BST* BST::search_for_item(BST* root, double price,std::string output)
+BST* BST::search_for_item(BST* root, double price, std::string output)
 {
     ofstream file_output;
     if (root == NULL)
     {
         return NULL;
     }
-    else
+    if (root->item.price <= price)
     {
-        if (root->item.price <= price)
-        {
-            file_output.open(output);
-            file_output << "There is this item:"<<root->item;
-            file_output.close();
-            return root;
-        }
-        search_for_item(root->left,price,output);
-        search_for_item(root->right,price,output);
+        file_output.open(output);
+        cout<< "There is this item:" << root->item;
+        file_output << "There is this item:" << root->item;
+        file_output.close();
+        return root;
     }
+    return search_for_item(root->left, price, output);
 }
-int* argument_parser(int nr_arguments,char** arguments)
-{   
-    int array_of_args [3],has_output_file=0,has_input_file=0;
+int* argument_parser(int nr_arguments, char** arguments)
+{
+    int array_of_args[3], has_output_file = 0, has_input_file = 0;
     for (int i = 0; i < nr_arguments; i++)
     {
-        if (strcmp(arguments[i], "-i")==0)
+        if (strcmp(arguments[i], "-i") == 0)
         {
-            array_of_args[0]=i + 1;
+            array_of_args[0] = i + 1;
             has_input_file = 1;
         }
         else if (strcmp(arguments[i], "-o") == 0)
@@ -88,9 +85,9 @@ int* argument_parser(int nr_arguments,char** arguments)
             has_output_file = 1;
         }
     }
-    if (has_output_file==0)
+    if (has_output_file == 0)
     {
-        cout<<"ERROR: missing output file";
+        cout << "ERROR: missing output file";
         return NULL;
     }
     else if (has_input_file == 0)
